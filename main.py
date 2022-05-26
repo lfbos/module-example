e5c8e3f565db14dc3a30e0b48642b2afdb13dc73
Luis Boscan <luis.boscan@globant.com>
2022-05-26 15:42:12 -0400

    Submodule added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from typing import Union
+import pendulum
 
+from typing import Union
 from fastapi import FastAPI
+
+from utils.dates import period_range
 
 app = FastAPI()
 
@@ -12,4 +15,9 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    today = pendulum.now()
+    one_month_ago = today.subtract(months=1)
+
+    date_range = list(period_range(today, one_month_ago, step_type="weeks"))
+
+    return {"total_weeks": len(date_range)}
